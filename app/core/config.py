ec04290a7313e45fe10613b3f070eb154d39b340
irfansd786 <irfansd4646@gmail.com>
2026-09-19 19:22:36 +0530

    chore: update documentation and cors origins

--- app/core/config.py
+++ app/core/config.py
@@ -24,9 +24,9 @@ class Settings(BaseSettings):
             'http://localhost:5173',
             'http://127.0.0.1:5173',
             'http://localhost:3000',
-            'http://127.0.0.1:3000',
             'http://localhost:8000',
             'http://127.0.0.1:8000',
+            'https://inventell-backend-rth.onrender.com',
         ]
         for orig in default_origins:
             origins.add(orig.rstrip('/'))
